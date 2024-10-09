de9a7f7fc9122bab02ed5709d788b3d7b5231fda
jzbor <jzbor@posteo.net>
2024-10-10 01:21:36 +0200

    Removing nixpkgs input

--- flake.nix
+++ flake.nix
@@ -1,58 +1,8 @@
 {
   description = "jzbor's flake framework";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
-  outputs = { self, nixpkgs }:
-  (((import ./lib.nix nixpkgs).flakeForDefaultSystems (system:
-  with builtins;
-  let
-    pkgs = nixpkgs.legacyPackages."${system}";
-    lib = import ./lib.nix nixpkgs;
-  in {
-
-    ### EXAMPLES ###
-    devShells = lib.createShells {
-      inherit system;
-      extraStdenvs.clang = pkgs.clangStdenv;
-    } {
-      example = {
-        nativeBuildInputs = [ pkgs.hello ];
-      };
-    };
-
-    packages = (lib.createPackages {
-      inherit system;
-      extraStdenvs.clang = pkgs.clangStdenv;
-    } {
-      example = {
-        nativeBuildInputs = [ pkgs.hello ];
-        src = "${self}";
-        installPhase = ''
-          mkdir -p $out
-          hello > $out/hello.out
-        '';
-      };
-    }) // (lib.packageAliases self system {
-      default = "example";
-    });
-
-    ### APPS ###
-    apps.gitlab-ci-discover = lib.createShellApp system {
-      name = "gitlab-ci-discover";
-      text = readFile ./scripts/gitlab-ci-discover.sh;
-      runtimeInputs = with pkgs; [ jq gnused ];
-    };
-
-    apps.add-gitlab-ci = lib.createShellApp system {
-      name = "add-gitlab-ci";
-      text = "cp -vi ${self}/gitlab-ci/gitlab-ci.yml .gitlab-ci.yml";
-    };
-  })) // {
+  outputs = { self }: {
     mkLib = import ./lib.nix;
-
     templates = import ./templates;
-  });
+  };
 }
-
-
-
