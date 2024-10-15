7f414797e74d109eaa437b0493386c433f74b185
jzbor <jzbor@posteo.net>
2024-10-15 11:30:10 +0200

    Adding rust template with clap

--- templates/default.nix
+++ templates/default.nix
@@ -9,6 +9,11 @@
     path = ./rust;
   };
 
+  rust-application = {
+    description = "Rust cornflakes template with clap";
+    path = ./rust-application;
+  };
+
   typst = {
     description = "Typst cornflakes template";
     path = ./typst;
